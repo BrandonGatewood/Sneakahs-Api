fbf7e29e412eb83e1e590cbe22ed84aa27e5aecc
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2023-05-04 17:21:24 -0700

    Starter package for spring application

---
+++ src/main/java/com/example/Sneakahs/SneakahsApi/SneakahsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sneakahs.SneakahsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakahsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakahsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sneakahs/SneakahsApi/SneakahsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sneakahs.SneakahsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakahsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
